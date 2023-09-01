58846f5f0ede2aa34bc2eacdd3e2bdcc50daeaec
omar karame <omarkarame21@gmail.com>
2023-09-01 13:53:05 +0300

    added new functions for testing ai message gen

--- commit_to_excellence.py
+++ commit_to_excellence.py
@@ -5,10 +5,17 @@ import os
 
 @click.group()
 def cte():
-    click.echo("")
+    pass
+
+
+def model_api_call():
+    return "AI generated message"
+
 
 # List of files that the user wants to commit
 files = []
+
+
 
 def find_git_directory():
     path = os.getcwd()
@@ -18,25 +25,19 @@ def find_git_directory():
         path = os.path.dirname(path)
     return None
 
+
+
 def commit_empty_message():
     repo_path = find_git_directory()
     repo = git.Repo(f'{repo_path}')
     index = repo.index
     index.commit("")
-    # for item in repo.index.diff(None):
-    #     click.echo(f'File {item.a_path} committed!')
+
 
 
 def add_file_to_list(file):
     """Simple program that allows the user to add a file to commit"""
     files.append(file)
-
-
-
-# https://github.com/OmarKarame/Commit-To-Excellence-Backend
-# OmarKarame/Commit-To-Excellence-Backend
-# /Users/omarkarame/code/OmarKarame/Commit-To-Excellence/Commit-To-Excellence-Backend
-# Code-Test-click-application-with-python-methods#34
 
 
 def connect_py():
@@ -45,6 +46,25 @@ def connect_py():
     repo = git.Repo(f'{repo_path}')
     # click.echo(f"Connected SUCCESSFULLY to: {repo}!")
     return repo
+
+
+def get_last_commit_hash():
+    repo = git.Repo('.')  # Initialize a Git repository object from the current directory
+    head_commit = repo.head.commit  # Get the latest (HEAD) commit
+
+    return head_commit.hexsha
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 @cte.command()
@@ -56,6 +76,7 @@ def connect():
     return repo
 
 
+
 @cte.command()
 @click.option('-b','--branch', prompt='Checkout to branch',
               help='The branch you would like to change to/ work on')
@@ -65,11 +86,13 @@ def checkout(branch):
     repo.git.checkout(branch)
 
 
+
 @cte.command()
 def status():
     """Simple program that prints git status"""
     repo = connect_py()
     click.echo(repo.git.status())
+
 
 
 @cte.command()
@@ -80,6 +103,7 @@ def add(file):
     repo.git.add(os.path.abspath(file))
 
 
+
 @cte.command()
 @click.option('-m','--message', prompt='Add message to commit',
               help='The commit message you want to add to the staged file')
@@ -88,23 +112,50 @@ def commit(message):
     repo = git.Repo(f'{repo_path}')
     index = repo.index
     index.commit(message)
-    click.echo(f"Success!")
+
+
+
+@cte.command()
+def ai_commit():
+    repo_path = find_git_directory()
+    repo = git.Repo(f'{repo_path}')
+    index = repo.index
+    index.commit("")
+
+    commit_hash = get_last_commit_hash()
+
+    commit = repo.commit(commit_hash)
+
+    new_message = model_api_call()
+
+    confirmation = click.confirm(f'''
+                 AI generated message:
+
+                 {new_message}
+
+                 Do you want to commit your file with this message? (y/N)
+
+                 ''')
+
+    if confirmation:
+        repo.git.commit(
+            '--amend', '-C', commit_hash, '-m', new_message, author=commit.author
+        )
+    else:
+        click.prompt('Please type int your new message: ', type=str)
+
+
 
 
 @cte.command()
 @click.option('-r','--remote', default='origin', help='Name of the remote repository.')
 @click.argument('branch')
 def push(remote, branch):
-    """
-    Push code to a GitHub repository.
-    """
     try:
-        repo = git.Repo('.')  # Initialize a Git repository object from the current directory
+        repo = git.Repo('.')
 
-        # Fetch the remote repository to ensure you have the latest changes
         repo.remotes[remote].fetch()
 
-        # Push the specified branch to the remote repository
         repo.remotes[remote].push(branch)
 
         click.echo(f'Successfully pushed branch {branch} to {remote}.')
@@ -114,10 +165,10 @@ def push(remote, branch):
 
 @cte.command()
 def get_diff():
-    """Simple program that allows the user to get info about staged files"""
+
     repo = connect_py()
     index = repo.index
-    # Get staged (added) files
+
     staged_files = [item.a_path for item in repo.index.diff(None)]
 
     diffs = repo.index.diff(None)
@@ -126,15 +177,11 @@ def get_diff():
         print("File:", diff.a_path)
         print("Change Type:", diff.change_type)
 
-        if diff.a_blob:  # Check if a_blob is not None
+        if diff.a_blob:
             print("Old Content:", diff.a_blob.data_stream.read())
 
-        if diff.b_blob:  # Check if b_blob is not None
+        if diff.b_blob:
             print("New Content:", diff.b_blob.data_stream.read())
 
         print("Diff:\n", diff.diff)
         print("-" * 40)
-
-
-# if __name__ == '__main__':
-#     get_diff()
